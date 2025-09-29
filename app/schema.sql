70571d93475b46dcb05578ed6bb1c8e4807d1c85
Isalys69 <isalys.creuzeau@gmail.com>
2025-09-29 13:41:11 +0200

    G0R8C1: Mise à jour schema.sql avec orders et order_lines

--- app/schema.sql
+++ app/schema.sql
@@ -6,17 +6,18 @@ CREATE TABLE vins (
     accord TEXT
 , domaine varchar(50), image varchar(255));
 CREATE TABLE sqlite_sequence(name,seq);
-CREATE TABLE orders(
-id INT,
+CREATE TABLE orders (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
 date DATE,
-total INT,
+total REAL,
 statut TEXT,
-client_id INT
+client_id INT);
+CREATE TABLE order_lines (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    order_id INT,
+    vin_id INT,
+    qty INT,
+    unit_price REAL,
+    FOREIGN KEY(order_id) REFERENCES orders(id),
+    FOREIGN KEY(vin_id) REFERENCES vins(id)
 );
-CREATE TABLE order_lines(
-order_id INT,
-vin_id INT,
-qty INT,
-unit_price REAL,
-FOREIGN KEY(order_id) REFERENCES orders(id),
-FOREIGN KEY(vin_id) REFERENCES vins(id));
